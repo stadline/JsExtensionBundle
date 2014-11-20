777adb4ca150af09ed4e541f16afa78dd5839d78
alex-pex <alexandre.paixao@stadline.com>
2014-11-20 11:22:43 +0100

    Modified App parameters to be a Model instead of a Collection

--- Resources/public/backbone/extensions/application.js
+++ Resources/public/backbone/extensions/application.js
@@ -2,16 +2,28 @@ Extension = window.Extension || {};
 
 Extension.Application = Marionette.Application.extend({
     initialize: function() {
+        var App = this;
+
         Marionette.Behaviors.behaviorsLookup = function() {
             return App.Behaviors;
         };
+
+        App.on("before:start", function() {
+            App.parameters = new Backbone.Model();
+        });
+
+        App.on("stop", function() {
+            App.parameters.clear();
+
+            delete App.parameters();
+        });
     },
 
     setParameter: function(key, value) {
-        App.parameters.setValue(key, value);
+        this.parameters.set(key, value);
     },
 
     getParameter: function(key) {
-        return App.parameters.getValue(key);
+        return this.parameters.get(key);
     }
 });
--- Resources/public/backbone/models/parameter.js
+++
@@ -1,9 +0,0 @@
-App = window.App || new Extension.Application();
-
-App.module('Models', function(module) {
-    module.Parameter = Backbone.Model.extend({
-        defaults: {
-            value: null
-        }
-    });
-});
